a9d1ea7d85820f412f56a11747da1006a7f2c463
jason <jason@mothership.(none)>
2013-11-12 20:49:52 -0800

    added readme and setup scripts

---
+++ installl.sh
@@ -0,0 +1,7 @@
+
+# Ubuntu based:
+
+sudo apt-get update
+sudo apt-get install -y vim build-essential python-software-properties    # The Basics
+sudo apt-get install -y python-opencv python-numpy python-scipy        # OpenCV item
+sudo apt-get install python-imaging
